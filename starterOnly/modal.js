44d66eb2a02a9328271464f4fe2214c815519721
LoicAnael <loicanaelbdf@gmail.com>
2022-02-19 22:24:05 +0400

    error style border set

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -33,7 +33,7 @@ const closeModal = () => {
 //close modal event
 closeBtn.forEach((element) => element.addEventListener("click", closeModal));
 
-//ISSUE 2 : VALIDATION FORM
+//ISSUE 2 + 3 + 4: VALIDATION FORM
 //DOM element
 const prenom = document.getElementById("first");
 const nom = document.getElementById("last");
@@ -42,8 +42,10 @@ const birthdate = document.getElementById("birthdate");
 const quantity = document.getElementById("quantity");
 const userCondition = document.querySelector("#checkbox1");
 const missingField = document.querySelector(".champ-manquant");
-const entries = document.querySelectorAll(".entries");
+const inputs = document.querySelectorAll(".entries");
 const sendBtn = document.getElementById("send");
+const form = document.getElementById("form");
+
 //regex
 const namesRegex = /^[A-Za-zçàéèÉÈÀÇ]{2,}$/;
 const emailRegex = /\S[a-zA-Z0-9]+@\S+\.\S+/;
@@ -57,72 +59,115 @@ const errNumber = document.querySelector(".err-number");
 const errLocation = document.querySelector(".err-location");
 const errCondition = document.querySelector(".err-condition");
 //error message if all fields are not filled
-const sendForm = (e) => {
-  let isFormValid = true;
-  for (let entry of entries) {
-    if (entry.validity.valueMissing) {
-      missingField.innerHTML = "<i>veillez remplir tout les champs</i>";
-      isFormValid = false;
-    } else {
-      missingField.innerHTML = "";
-    }
-    e.preventDefault();
-  }
 
-  //error message for incorrect inputs
-  if (namesRegex.test(prenom.value) == false) {
-    errFistName.innerHTML = "<i>Format de prenom incorrect</i>";
+//error message for incorrect inputs
+let isFormValid = true;
+const errorFocusName = () => {
+  if (!namesRegex.test(prenom.value)) {
+    errFistName.innerHTML =
+      "Veuillez entrer au minimum 2 caractères pour ce champ";
+    prenom.style.borderColor = "#F00";
     isFormValid = false;
   } else {
     errFistName.innerHTML = "";
+    prenom.style.borderColor = "#FFF";
   }
-  if (namesRegex.test(nom.value) == false) {
-    errLastName.innerHTML = "<i>Format de nom incorrect</i>";
+  return isFormValid;
+};
+const errorFocusSurname = () => {
+  if (!namesRegex.test(nom.value)) {
+    errLastName.innerHTML =
+      "Veuillez entrer au minimum 2 caractères pour ce champ";
+    nom.style.borderColor = "#F00";
     isFormValid = false;
   } else {
     errLastName.innerHTML = "";
+    nom.style.borderColor = "#FFF";
   }
-  if (emailRegex.test(email.value) == false) {
-    errEmail.innerHTML = "<i>Format d'adresse E-mail incorrect";
+  return isFormValid;
+};
+const errorFocusEmail = () => {
+  if (!emailRegex.test(email.value)) {
+    errEmail.innerHTML = "Format d'adresse E-mail incorrect";
+    email.style.borderColor = "#F00";
     isFormValid = false;
   } else {
     errEmail.innerHTML = "";
+    email.style.borderColor = "#FFF";
   }
+  return isFormValid;
+};
+const errorFocusBirthdate = () => {
   if (!birthdate.value) {
     errBirthdate.innerHTML = "Vous devez entrer votre date de naissance";
+    birthdate.style.borderColor = "#F00";
     isFormValid = false;
   } else {
     errBirthdate.innerHTML = "";
+    birthdate.style.borderColor = "#FFF";
   }
-  if (numbersRegex.test(quantity.value) == false) {
-    errNumber.innerHTML = "<i>Format de saisie incorrect</i>";
+  return isFormValid;
+};
+const errorFocusNumber = () => {
+  if (!numbersRegex.test(quantity.value)) {
+    errNumber.innerHTML = "Veuillez entrer des chiffres uniquement";
+    quantity.style.borderColor = "#F00";
     isFormValid = false;
   } else {
     errNumber.innerHTML = "";
+    quantity.style.borderColor = "#FFF";
   }
+  return isFormValid;
+};
+const errorFocusLocation = () => {
   const locations = document.querySelectorAll("[name=location]:checked");
   if (locations.length < 1) {
-    errLocation.innerHTML = "</br><i>Veillez faire un choix</i>";
+    errLocation.innerHTML = "Veillez faire un choix";
     isFormValid = false;
   } else {
     errLocation.innerHTML = "";
   }
-
+  return isFormValid;
+};
+const errorFocusCondition = () => {
   if (!userCondition.checked) {
-    errCondition.innerHTML = "<i>Champ obligatoire";
+    errCondition.innerHTML =
+      "</br>Vérifiez que vous acceptez les termes et conditions !";
     isFormValid = false;
   } else {
     errCondition.innerHTML = "";
   }
+  return isFormValid;
+};
+function sendForm(e) {
+  e.preventDefault();
+  errorFocusCondition();
+  errorFocusName();
+  errorFocusSurname();
+  errorFocusEmail();
+  errorFocusBirthdate();
+  errorFocusNumber();
+  errorFocusLocation();
   if (isFormValid) {
-    const modalbg = document.querySelector(".bground");
-    const thanksModalBg = document.querySelector(".modal-confirm");
+    let modalbg = document.querySelector(".bground");
+    let thanksModalBg = document.querySelector(".modal-confirm");
     modalbg.style.display = "none";
     thanksModalBg.style.display = "block";
-    e.preventDefault();
+  } else if (!isFormValid) {
+    modalbg.style.display = "block";
+    thanksModalBg.style.display = "none";
   }
-  //return isFormValid;
+  return isFormValid;
+}
+//send form event
+form.addEventListener("submit", sendForm);
+
+//reset form after validation
+const resetBtn = document.getElementById("reset");
+const entries = document.querySelectorAll(".entries");
+const reset = () => {
+  entries.forEach((input) => (input.value = ""));
+  thanksModalBg.style.display = "none";
 };
 
-//send form event
-sendBtn.addEventListener("click", sendForm);
+resetBtn.addEventListener("click", reset);
